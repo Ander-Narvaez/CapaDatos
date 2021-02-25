99dda14175d3081f46117e303b595e05f1529f53
Ander <anarvaezgo@est.utn.ac.cr>
2021-02-25 12:21:51 -0600

    Clase persona, atributos y constructor

--- Clases/ClsPersonas.cs
+++ Clases/ClsPersonas.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace CapaDatos
 {
-    public class Class1
+    public class ClsPersonas
     {
+        public String aNif;
+        public String aNombre_Completo;
+        public String aDireccion;
+        public String aTelefono;
+        public String aPoblacion;
+        public String aProvincia;
+        public int    aCodigo_Postal;
+        public String aNum_Seguridad_Social;
+
+        public ClsPersonas()
+        {
+            this.aNif                  = "";
+            this.aNombre_Completo      = "";
+            this.aDireccion            = "";
+            this.aTelefono             = "";
+            this.aPoblacion            = "";
+            this.aProvincia            = "";
+            this.aCodigo_Postal        = 0;
+            this.aNum_Seguridad_Social = "";
+            
+        }
+
+        public ClsPersonas(string pNif, String pNombre_Completo, String pDireccion, String pTelefono, String pPoblacion, String pProvincia, int pCodigo_Postal, String pNum_Seguridad_Social )
+        {
+            this.aNif                  = pNif;
+            this.aNombre_Completo      = pNombre_Completo;
+            this.aDireccion            = pDireccion;
+            this.aTelefono             = pTelefono;
+            this.aPoblacion            = pPoblacion;
+            this.aProvincia            = pProvincia;
+            this.aCodigo_Postal        = pCodigo_Postal;
+            this.aNum_Seguridad_Social = pNum_Seguridad_Social;
+        }
     }
 }
