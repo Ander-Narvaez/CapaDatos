c4c80da90b299778b75781c10e3b9f26462839c0
Ander <anarvaezgo@est.utn.ac.cr>
2021-03-02 06:55:53 -0600

    cambio1

--- Clases/ClsHorarios.cs
+++ Clases/ClsHorarios.cs
@@ -8,24 +8,27 @@ using System.Threading.Tasks;
 
 namespace CapaDatos.Clases
 {
-    public class ClsPacientes : ClsConexion
+    public class ClsHorarios : ClsConexion
     {
-        public String aNif_Paciente;
+        public String aDia;
+        public String aJornada;
         public String aMedico;
 
-        public ClsPacientes()
+        public ClsHorarios()
         {
-            this.aNif_Paciente = "";
-            this.aMedico       = "";
+            this.aDia     = "";
+            this.aJornada = "";
+            this.aMedico  = "";
         }
 
-        public ClsPacientes(string pNif_Paciente, String pMedico)
+        public ClsHorarios(string pDia, String pJornada, String pMedico)
         {
-            this.aNif_Paciente = pNif_Paciente;
-            this.aMedico       = pMedico;
+            this.aDia     = pDia;
+            this.aJornada = pJornada;
+            this.aMedico  = pMedico;
         }
 
-        public String MantenimientoPacientes(ClsPacientes pClspacientes, String pAccion)
+        public String MantenimientoHorarios(ClsHorarios pClshorarios, String pAccion)
         {
             String vResultado = "";
             if (this.Conectando())
@@ -37,10 +40,11 @@ namespace CapaDatos.Clases
                     SqlCommand coneccion = new SqlCommand();
                     coneccion.Connection = conectado;
                     coneccion.CommandType = CommandType.StoredProcedure;
-                    coneccion.CommandText = "Stp_MantenimientoPacientes";
+                    coneccion.CommandText = "stp_MantenimientoHorarios";
                     coneccion.CommandTimeout = 10;
-                    coneccion.Parameters.AddWithValue("@pNif_Paciente", pClspacientes.aNif_Paciente);
-                    coneccion.Parameters.AddWithValue("@pMedico", pClspacientes.aMedico);
+                    coneccion.Parameters.AddWithValue("@pDia", pClshorarios.aDia);
+                    coneccion.Parameters.AddWithValue("@pJornada", pClshorarios.aJornada);
+                    coneccion.Parameters.AddWithValue("@pMedico", pClshorarios.aMedico);
                     coneccion.Parameters.AddWithValue("@pAccion", pAccion);
                     coneccion.ExecuteNonQuery();
                     conectado.Close();
@@ -56,7 +60,7 @@ namespace CapaDatos.Clases
         }
 
         private DataSet dataTable = new DataSet();
-        public DataSet GetListaPacientes(ClsPacientes pClspacientes, String pAccion)
+        public DataSet GetListaHorarios(ClsHorarios pClshorarios, String pAccion)
         {
             try
             {
@@ -67,9 +71,10 @@ namespace CapaDatos.Clases
                 SqlCommand coneccion = new SqlCommand();
                 coneccion.Connection = conectado;
                 coneccion.CommandType = CommandType.StoredProcedure;
-                coneccion.CommandText = "Stp_MantenimientoPacientes";
-                coneccion.Parameters.AddWithValue("@pNif_Paciente", pClspacientes.aNif_Paciente);
-                coneccion.Parameters.AddWithValue("@pMedico", pClspacientes.aMedico);
+                coneccion.CommandText = "Stp_MantenimientoHorarios";
+                coneccion.Parameters.AddWithValue("@pDia", pClshorarios.aDia);
+                coneccion.Parameters.AddWithValue("@pJornada", pClshorarios.aJornada);
+                coneccion.Parameters.AddWithValue("@pMedico", pClshorarios.aMedico);
                 coneccion.Parameters.AddWithValue("@pAccion", pAccion);
                 adapter = new SqlDataAdapter(coneccion);
                 adapter.Fill(dataTable);
